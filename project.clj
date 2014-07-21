5df3739b63983003fe3a811bfc495362da2ae240
Allen Rohner <arohner@gmail.com>
2014-07-21 14:01:40 -0500

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject defpage "0.1.2"
+(defproject defpage "0.1.3"
   :description "A simple way to generate ring handlers"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
