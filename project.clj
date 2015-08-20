45bd98f83547a809113f1a20b5aa46e3687491c5
Allen Rohner <arohner@gmail.com>
2015-08-20 18:32:40 -0500

    project.clj changes to fix `lein release`

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject defpage "0.1.3"
   :description "A simple way to generate ring handlers"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/arohner/defpage"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.3.0"]
                  [clout "1.2.0"]
-                 [compojure "1.1.8"]])
+                 [compojure "1.1.8"]]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :creds :gpg}]])
